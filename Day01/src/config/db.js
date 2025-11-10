6c1fcc919a548f04028ccda9f0f26ed5562ec661
Aditya Ashok Singh <adityaashoksingh7@gmail.com>
2025-11-10 22:45:13 +0530

    fixing

--- Day01/src/config/db.js
+++
@@ -1,5 +0,0 @@
-import mongoose from 'mongoose'
-async function main() {
-    await mongoose.connect(process.env.DB_CONNECT_STRING)
-}
-export default main
--- Day01/src/routes/problemCreator.js
+++
@@ -1,8 +0,0 @@
-import express from 'express'
-const problemRouter=express.Router()
-problemRouter.post("/create",problemCreate)
-problemRouter.get("/:id",problemfetch)
-problemRouter.get("/",problemFetchAll)
-problemRouter.get("/user",solvedPrblem)
-problemRouter.patch("/:id",problemUpdate)
-problemRouter.delete("/:id",problemDelete)
