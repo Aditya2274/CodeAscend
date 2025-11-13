ee4473aef38c9c0ded15259f932f1a4fa29d206d
Aditya Ashok <adityaashoksingh7@gmail.com>
2025-11-14 00:50:55 +0530

    Restore Frontend as normal folder

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import Login from "./pages/Login"
+import SignUp from "./pages/SignUp"
+import HomePage from "./pages/HomePage"
+function App(){
+  return(
+    <>
+    <Routes>
+      <Route path="/" element={<HomePage></HomePage>}></Route>
+      <Route path="/login" element={<Login></Login>}></Route>
+      <Route path="/signup" element={<SignUp></SignUp>}></Route>
+    </Routes>
+    </>
+  )
+}
+export default App
